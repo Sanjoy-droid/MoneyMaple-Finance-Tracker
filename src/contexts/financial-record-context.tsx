b31d7e07a85f2d5acc4409c4f016db2ed47eff7f
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-06-29 17:48:03 +0530

    feat: Implement fetch records function in frontend

--- src/contexts/financial-record-context.tsx
+++ src/contexts/financial-record-context.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const PORT = import.meta.env.VITE_PORT;
 interface FinancialRecord {
@@ -28,6 +29,34 @@ export const FinancialRecordsProvider = ({
   children: React.ReactNode;
 }) => {
   const [records, setRecords] = useState<FinancialRecord[]>([]);
+
+  const { user } = useUser();
+
+  const fetchRecords = async () => {
+    if (!user) return;
+    try {
+      const response = await fetch(
+        `${PORT}/financial-records/getAllByUserId/${user?.id}`
+      );
+      if (response.ok) {
+        const records = await response.json();
+        console.log(records);
+        setRecords(records);
+      } else {
+        console.error("Failed to fetch records:", response.statusText);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error fetching records:", error.message);
+      } else {
+        console.error("Unexpected error fetching records:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
     const response = await fetch(`${PORT}/financial-records`, {
